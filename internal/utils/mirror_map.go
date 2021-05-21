426e1266703b8ad1a34db78ea2d5f2da460e1bd2
Sergey Pislegin <serg.pislegin@gmail.com>
2021-05-21 19:59:27 +0300

    Fix function description

--- internal/utils/mirror_map.go
+++ internal/utils/mirror_map.go
@@ -5,7 +5,7 @@ import "errors"
 // MirrorMap creates a new map in a way that values of the old map become
 // keys of the new map, and keys of the old map become values of the new map
 // If passed map is nil, error will be returned.
-// If original map contains two equal values, error will be returned
+// If original map contains two equal values, panic will be invoked
 func MirrorMap(dict map[interface{}]interface{}) (map[interface{}]interface{}, error) {
 	if dict == nil {
 		return nil, errors.New("cannot mirror nil map")

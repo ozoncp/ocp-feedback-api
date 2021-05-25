ccddb97d795f3697a4e20c0126b2c9ca854f51c8
Sergey Pislegin <serg.pislegin@gmail.com>
2021-05-25 21:44:21 +0300

    Add file observer

--- cmd/ocp-feedback-api/main.go
+++ cmd/ocp-feedback-api/main.go
@@ -1,12 +1,92 @@
 package main
 
-import "fmt"
-
-// Hello sends greetings to the user with a given name
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %v!", name)
-}
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println(Hello("Sergey Pislegin"))
+	consumerTimeout, sleepTime := time.Second*2, time.Second*1
+	sc := &SleepyConsumer{os.Stdout, consumerTimeout}
+	if err := ObserveFile("some_file.txt", sc, sleepTime); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Sleeper is an interface that represents operation delay
+type Sleeper interface {
+	Sleep()
+}
+
+// SleepyConsumer consumes bytes with a defined delay
+type SleepyConsumer struct {
+	io.Writer
+	sleepDuration time.Duration
+}
+
+// Sleep pauses execution for a certain period of time
+func (c *SleepyConsumer) Sleep() {
+	time.Sleep(c.sleepDuration)
+}
+
+// Write wraps around Writer's write method and adds some detailed output
+func (c *SleepyConsumer) Write(p []byte) (n int, err error) {
+	fmt.Printf("Consumed %v bytes from file. Contents are: \n", len(p))
+	return c.Writer.Write(p)
+}
+
+// ObserveFile opens file and keeps reading its contents over and over
+// Every time file contents are read, consumer will be notified
+// Sleep method is used to add a delay between file reads
+func ObserveFile(fileName string, consumer *SleepyConsumer, timeout time.Duration) error {
+	if consumer == nil {
+		panic("consumer can't be nil")
+	}
+
+	readFile := func() error {
+		return readFileContents(fileName, consumer, timeout)
+	}
+
+	for {
+		if err := readFile(); err != nil {
+			return fmt.Errorf("unable to read file %v: %v", fileName, err)
+		}
+		consumer.Sleep()
+	}
+}
+
+// readFileContents opens file, reads its contents and passes the contents to the consumer
+// if consumer is busy, function will try to wait until all chunks are consumed
+// timeout is used to control amount of time given to consume the data
+func readFileContents(fileName string, writer io.Writer, timeout time.Duration) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	totalSize := 0
+	for leave, timeout_guard := false, time.After(timeout); !leave; {
+		select {
+		case <-timeout_guard:
+			leave = true // consumer timed out
+		default:
+			chunkSize, err := writer.Write(data[totalSize:])
+			if err != nil {
+				return err
+			}
+			if totalSize += chunkSize; totalSize >= len(data) {
+				return nil // consumer consumed all the data
+			}
+		}
+	}
+	return errors.New("time is up")
 }
--- cmd/ocp-feedback-api/main_test.go
+++ cmd/ocp-feedback-api/main_test.go
@@ -1,12 +1,133 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+)
 
-func TestHello(t *testing.T) {
-	got := Hello("John")
-	want := "Hello, John!"
+// spyConsumer is a consumer stub that allows to examine consumed data
+type spyConsumer struct {
+	bytes []byte
+}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+func (r *spyConsumer) Write(p []byte) (n int, err error) {
+	consumedElements := 1 // consume 1 element at a time
+	r.bytes = append(r.bytes, p[:consumedElements]...)
+	return consumedElements, nil
+}
+
+// busyConsumer is a consumer stub that imitates non-responding consumer
+type busyConsumer struct{}
+
+func (r *busyConsumer) Write(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+// brokenComsumer is a consumer stub that returns an error
+type brokerConsumer struct{}
+
+func (r *brokerConsumer) Write(p []byte) (n int, err error) {
+	return len(p), errors.New("i'm broken :(")
+}
+
+func TestReadFile(t *testing.T) {
+	t.Run("empty file name", func(t *testing.T) {
+		sleepTime, consumerTimeout := time.Second, time.Second
+		sc := &SleepyConsumer{os.Stdout, sleepTime}
+		err := readFileContents("", sc, consumerTimeout)
+
+		assertNonNilError(t, err)
+	})
+
+	t.Run("wrong file name", func(t *testing.T) {
+		sleepTime, consumerTimeout := time.Second, time.Second
+		sc := &SleepyConsumer{os.Stdout, sleepTime}
+		err := readFileContents("invalid_filename", sc, consumerTimeout)
+
+		assertNonNilError(t, err)
+	})
+
+	t.Run("nil consumer", func(t *testing.T) {
+		fileName, deleteFile := createTempFile(t, "some_file", "")
+		defer assertPanic(t)
+		defer deleteFile()
+
+		_ = ObserveFile(fileName, nil, time.Second)
+		t.Error("goroutine must enter panic state")
+	})
+
+	t.Run("read file contents", func(t *testing.T) {
+		consumer := &spyConsumer{}
+		file_contents := "file_contents"
+		fileName, deleteFile := createTempFile(t, "file_name", file_contents)
+		defer deleteFile()
+		err := readFileContents(fileName, consumer, time.Second*10)
+
+		assertNilError(t, err)
+
+		if !reflect.DeepEqual(consumer.bytes, []byte(file_contents)) {
+			t.Errorf("got %v want %v", consumer.bytes, []byte(file_contents))
+		}
+	})
+
+	t.Run("consumer timeout", func(t *testing.T) {
+		consumer := &busyConsumer{}
+		file_contents := "file_contents"
+		fileName, deleteFile := createTempFile(t, "file_name", file_contents)
+		defer deleteFile()
+		err := readFileContents(fileName, consumer, time.Millisecond)
+
+		assertNonNilError(t, err)
+	})
+
+	t.Run("consumer error", func(t *testing.T) {
+		consumer := &brokerConsumer{}
+		file_contents := "file_contents"
+		fileName, deleteFile := createTempFile(t, "file_name", file_contents)
+		defer deleteFile()
+		err := readFileContents(fileName, consumer, time.Second*10)
+
+		assertNonNilError(t, err)
+	})
+}
+
+func createTempFile(t *testing.T, fileName string, initialData string) (string, func()) {
+	t.Helper()
+	tmpfile, err := ioutil.TempFile("", fileName)
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+	return tmpfile.Name(), removeFile
+}
+
+func assertNonNilError(t *testing.T, err error) {
+	t.Helper()
+
+	if err == nil {
+		t.Errorf("error must be returned")
+	}
+}
+
+func assertNilError(t *testing.T, err error) {
+	t.Helper()
+
+	if err != nil {
+		t.Errorf("error must not be returned")
+	}
+}
+
+func assertPanic(t *testing.T) {
+	if r := recover(); r == nil {
+		t.Errorf("goroutine must enter panic state")
 	}
 }

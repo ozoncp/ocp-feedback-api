71c9c80aa0b6a29dd04197b7d09a2202da79aec3
Sergey Pislegin <serg.pislegin@gmail.com>
2021-05-28 14:25:19 +0300

    Add flusher implementation

--- internal/flusher/flusher.go
+++ internal/flusher/flusher.go
@@ -1,7 +1,53 @@
 package flusher
 
-import "github.com/ozoncp/ocp-feedback-api/internal/models"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/ozoncp/ocp-feedback-api/internal/models"
+	"github.com/ozoncp/ocp-feedback-api/internal/repo"
+	"github.com/ozoncp/ocp-feedback-api/internal/utils"
+)
 
 type Flusher interface {
-	Flush(entities []models.Entity) []models.Entity
+	Flush(entities []models.Entity) ([]models.Entity, error)
+}
+
+type flusher struct {
+	chunkSize int
+	repo      repo.Repo
+}
+
+// New returns a new flusher object
+func New(chunkSize int, repo repo.Repo) (*flusher, error) {
+	if chunkSize < 0 {
+		return nil, errors.New("chunk size cannot be negative")
+	}
+	if repo == nil {
+		return nil, errors.New("repo cannot be nil")
+	}
+	return &flusher{chunkSize, repo}, nil
+}
+
+// Flush tries to push given entities into the repo
+// Entities that can't be pushed will be returned along with an error
+func (f *flusher) Flush(entities []models.Entity) ([]models.Entity, error) {
+	chunks, err := utils.SplitSlice(entities, f.chunkSize)
+	if err != nil {
+		return flattenEntities(chunks), fmt.Errorf("unable to flush: %v", err)
+	}
+
+	for i := 0; i < len(chunks); i++ {
+		if err := f.repo.AddEntities(chunks[i]); err != nil {
+			return flattenEntities(chunks[i:]), fmt.Errorf("unable to flush: %v", err)
+		}
+	}
+	return nil, nil // all entities have been flushed successfully
+}
+
+func flattenEntities(entities [][]models.Entity) (flattened []models.Entity) {
+	for i := 0; i < len(entities); i++ {
+		flattened = append(flattened, entities[i]...)
+	}
+	return
 }

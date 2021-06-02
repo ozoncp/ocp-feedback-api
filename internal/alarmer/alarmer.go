a0f59be24fbe520544d5cee9f0ba5a003bd7db15
Sergey Pislegin <serg.pislegin@gmail.com>
2021-06-02 20:50:02 +0300

    Fix naming

--- internal/alarmer/alarmer.go
+++ internal/alarmer/alarmer.go
@@ -11,15 +11,15 @@ type Alarmer interface {
 
 type void struct{}
 
-type alarmer struct {
+type alarm struct {
 	duration time.Duration
 	alarms   chan void
 	done     chan void
 }
 
 // New returns a new alarmer object
-func New(duration time.Duration) *alarmer {
-	return &alarmer{
+func New(duration time.Duration) *alarm {
+	return &alarm{
 		duration: duration,
 		alarms:   make(chan void),
 		done:     make(chan void),
@@ -28,7 +28,7 @@ func New(duration time.Duration) *alarmer {
 
 // Init starts repeatedly delivering asynchronous
 // notifications at regular intervals until Close is called
-func (a *alarmer) Init() {
+func (a *alarm) Init() {
 	go func() {
 		ticker := time.NewTicker(a.duration)
 		defer close(a.alarms)
@@ -46,11 +46,11 @@ func (a *alarmer) Init() {
 }
 
 // Alarm provides an access to notification channel
-func (a *alarmer) Alarm() <-chan void {
+func (a *alarm) Alarm() <-chan void {
 	return a.alarms
 }
 
 // Close notifies alarmer that no more alarms should be delivered
-func (a *alarmer) Close() {
+func (a *alarm) Close() {
 	close(a.done)
 }

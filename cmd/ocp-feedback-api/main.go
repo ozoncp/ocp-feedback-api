ab3a0f4a227c99b6204e8cb66a14750bc86a7631
Sergey Pislegin <serg.pislegin@gmail.com>
2021-06-07 22:08:42 +0300

    Add grcp server creation

--- cmd/ocp-feedback-api/main.go
+++ cmd/ocp-feedback-api/main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+
+	grpc_server "github.com/ozoncp/ocp-feedback-api/internal/server"
+	fb "github.com/ozoncp/ocp-feedback-api/pkg/ocp-feedback-api"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+)
+
+var grpcPort int
+
+func init() {
+	flag.IntVar(&grpcPort, "port", 10000, "GRPC server port")
+}
+
 func main() {
+	flag.Parse()
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	grpcEndpoint := fmt.Sprintf("localhost:%d", grpcPort)
+
+	lis, err := net.Listen("tcp", grpcEndpoint)
+	if err != nil {
+		log.Fatal().Err(err).Msgf("Cannot start feedback grpc server at %v", grpcEndpoint)
+	}
+	log.Info().Msgf("Starting server at %v...", grpcEndpoint)
+
+	grpcServer := grpc.NewServer()
+	fb.RegisterOcpFeedbackApiServer(grpcServer, grpc_server.New())
+
+	if err = grpcServer.Serve(lis); err != nil {
+		log.Fatal().Err(err).Msg("Cannot accept connections")
+	}
+
 }

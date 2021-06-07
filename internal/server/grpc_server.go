224bb55c24272b08546167b193a47918f325ffb2
Sergey Pislegin <serg.pislegin@gmail.com>
2021-06-07 20:31:10 +0300

    Update feedback messages

--- internal/server/grpc_server.go
+++ internal/server/grpc_server.go
@@ -17,24 +17,25 @@ func New() *grpcServer {
 
 // CreateFeedbackV1 ...
 func (s *grpcServer) CreateFeedbackV1(ctx context.Context, req *fb.CreateFeedbackV1Request) (*fb.CreateFeedbackV1Response, error) {
-	//log.Info("Handle request for GetRate", "base", rr.GetBase(), "dest", rr.GetDestination())
-	return &fb.CreateFeedbackV1Response{Id: 42}, nil
+	return &fb.CreateFeedbackV1Response{FeedbackId: 42}, nil
 }
 
 // RemoveFeedbackV1 ...
 func (s *grpcServer) RemoveFeedbackV1(ctx context.Context, req *fb.RemoveFeedbackV1Request) (*fb.RemoveFeedbackV1Response, error) {
-	// TODO use grpc status code to indicate id_not_found
+	// TODO return codes.NotFound if requsted id is not found
 	return &fb.RemoveFeedbackV1Response{}, nil
 }
 
 // DescribeFeedbackV1 ...
 func (s *grpcServer) DescribeFeedbackV1(ctx context.Context, req *fb.DescribeFeedbackV1Request) (*fb.DescribeFeedbackV1Response, error) {
-	dummy := &fb.Feedback{Id: 42, UserId: 100, ClassroomId: 200, Comment: "just_a_comment"}
+	// TODO return codes.NotFound if requsted id is not found
+	dummy := &fb.Feedback{FeedbackId: 42, UserId: 100, ClassroomId: 200, Comment: "just_a_comment"}
 	return &fb.DescribeFeedbackV1Response{Feedback: dummy}, nil
 }
 
 // ListFeedbacksV1 ...
 func (s *grpcServer) ListFeedbacksV1(ctx context.Context, req *fb.ListFeedbacksV1Request) (*fb.ListFeedbacksV1Response, error) {
-	dummy := []*fb.Feedback{{Id: 42, UserId: 100, ClassroomId: 200, Comment: "just_a_comment"}}
+	// TODO:: return codes.OutOfRange if provided offset is invalid
+	dummy := []*fb.Feedback{{FeedbackId: 42, UserId: 100, ClassroomId: 200, Comment: "just_a_comment"}}
 	return &fb.ListFeedbacksV1Response{Feedbacks: dummy}, nil
 }
